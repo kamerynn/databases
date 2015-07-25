21cf9f771a99bcd81e17dfd65776f22726c3ab83
Kamerynn Harrah <kamharrah8@gmail.com>
2015-07-24 20:16:05 -0400

    Add basic DB logic and controller POST logic

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -5,8 +5,30 @@ var bluebird = require('bluebird');
 
 module.exports = {
   messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
-    post: function (req, res) {} // a function which handles posting a message to the database
+    get: function (req, res) {
+      // a function which handles a get request for all messages
+    },
+    post: function (req, res) {
+      console.log('POST RECEIVED');
+
+      // already parsed from parser.json in app.js
+      var msg = req.body;
+      console.log('msg', msg);
+
+      // a function which handles posting a message to the database
+      // call post method on model, pass in data to write
+
+      if (models.messages.post(msg)) {
+      // if successful write
+        // express#send method for status code/headers (201 - Created)
+        // in response, pass the *Obj.id back* (maybe a refactor)
+        console.log('written!');
+      } else {
+        // express#send 400 - Bad Request
+        console.log('failed to write!');
+        // (may need to send object back)
+      }
+    }
   },
 
   users: {
