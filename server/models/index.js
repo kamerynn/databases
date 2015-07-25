83ad8ea5c4733b0e8e428bea6a5befa656709c11
Kamerynn Harrah <kamharrah8@gmail.com>
2015-07-24 23:02:49 -0400

    Implement synchronous query in model for POST

--- server/models/index.js
+++ server/models/index.js
@@ -1,12 +1,41 @@
 var db = require('../db');
 
-
-
-
 module.exports = {
   messages: {
     get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+
+    // a function which can be used to insert a message into the database
+    post: function (data) {
+
+      var connection = db.dbConnection();
+      connection.connect();
+
+      // MySQL only accepts a certain format for Date, JS does not have a native format method
+      var d = new Date,
+      dformat = [d.getFullYear(),
+              d.getMonth()+1,
+              d.getDate()].join('-')+' '+
+              [d.getHours(),
+              d.getMinutes(),
+              d.getSeconds()].join(':');
+
+      var writeSuccess, result;
+      // build query string using data to enter data into messages table on db
+      var queryString = "INSERT INTO messages (text, room_id, created_at, user_id) " +
+                        "VALUES ('" + data.text + "', 1, '" + dformat + "', " +
+                        "1)";
+
+      // attempting to write to db
+      // IS THE ASSIGNMENT HERE MAKING IT A SYNCHRONOUS OPERATION? - HELP DESK
+      result = connection.query(queryString, function(err, results) {
+        writeSuccess = err ? false : true;
+        console.log('SUCCESS?', writeSuccess);
+        return writeSuccess;
+      });
+
+      connection.end();
+      return result;
+    }
   },
 
   users: {
@@ -15,4 +44,3 @@ module.exports = {
     post: function () {}
   }
 };
-
